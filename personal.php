51aa7ebdec329aaafab82257bfd47bad159bfd82
imamhossain07 <134435063+imamhossain07@users.noreply.github.com>
2023-09-18 22:05:21 +0600

    Update personal.php

--- personal.php
+++ personal.php
@@ -5,7 +5,7 @@
         $name = "Imam Hossain";
         $age = 32;
         $country = "Bangladesh";
-        $introduction = "Hello, my name is Akmal Khan. I am $age years old and I am from Dhaka. It's a pleasure to meet you!";
+        $introduction = "Hello, my name is Imam Hossain. I am $age years old and I am from Dhaka. It's a pleasure to meet you!";
         
         echo "Name: $name<br>";
         echo "Age: $age<br>";
