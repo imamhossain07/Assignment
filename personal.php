614fd9b8334da1ccfc8707c6ab41aca770d7933a
imamhossain07 <134435063+imamhossain07@users.noreply.github.com>
2023-09-18 22:04:37 +0600

    Update personal.php

--- personal.php
+++ personal.php
@@ -2,7 +2,7 @@
 
     // Task 1: Personal Information Page
 
-        $name = "Akmal Khan";
+        $name = "Imam Hossain";
         $age = 32;
         $country = "Bangladesh";
         $introduction = "Hello, my name is Akmal Khan. I am $age years old and I am from Dhaka. It's a pleasure to meet you!";
